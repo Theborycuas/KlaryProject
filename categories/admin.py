bd08e950d703930a80d742e97b6916fc83a733fa
Borys <borysjair96@gmail.com>
2022-03-17 12:31:53 -0500

    Listo Api App Categories

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'update')
+
+admin.site.register(Category, CategoryAdmin)
